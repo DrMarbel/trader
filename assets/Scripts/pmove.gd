3b64ec392c5dab4010b0264e6c044e7696ee9f4a
DrMarbel <martinbelt753@gmail.com>
2024-02-16 11:43:22 -0600

    Basic player movement

--- assets/Scripts/pmove.gd
+++ assets/Scripts/pmove.gd
@@ -1,23 +1,11 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
-export (int) var speed = 200
-
-var velocity = Vector2()
+@export var speed = 400
 
 func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-		$Sprite.flip_h = 0
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-		$Sprite.flip_h = 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
-	velocity = move_and_slide(velocity)
+	move_and_slide()
